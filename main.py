da9cfbec9fac9068660002ec9ff497844eda772b
Aleksandr D <none>
2024-03-30 17:09:23 +0700

    almost nothing was changed

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from cv_html_generator import build
 from database.db_test import data as db
 
 
-file_css = "../css/cv_helion.css"
+# file_css = "../css/cv_helion.css"
+file_css = "../css/cv.css"
 file_cv = "output/cv.html"
 
 
@@ -15,7 +16,7 @@ if __name__ == '__main__':
             file_css,
             file_cv,
             db,
-            "Experiment & Test Engineer, Plasmas",
+            "Data Scientist",
             "th"  #th, uz_beeline, uz_mobile
         )
         webbrowser.open_new_tab("".join(["file:///", os.getcwd(), "/", file_cv]))
