01771149f42f4500821da3487f41e7c003ef6b7d
Aleksandr D <none>
2024-03-30 19:00:56 +0700

    add data check at each block

--- cv_html_generator.py
+++ cv_html_generator.py
@@ -27,209 +27,230 @@
 
 # NAME AND JOB TITLE
 def html_header(data_name, data_job_title):
-    return f'''
-        <div class="head_container">
-            <div class="name">
-                <h1>{data_name}</h1>
+    if any([data_name, data_job_title]):
+        return f'''
+            <div class="head_container">
+                <div class="name">
+                    <h1>{data_name}</h1>
+                </div>
+                <div class="job-title">
+                    <h2>{data_job_title}</h2>
+                </div>
             </div>
-            <div class="job-title">
-                <h2>{data_job_title}</h2>
-            </div>
-        </div>
-        <div class="vline"></div>
-    '''
+            <div class="vline"></div>
+        '''
+    else:
+        return ""
 
 
 # CONTACT INFO
-def html_contactinfo(data_email, data_phone, data_github, data_linkedin):
-    data_contactinfo_html = f'''
-            <table>
-                <tr>
-                    <td>Email:</td>
-                    <td><a href="{data_email}">{data_email}</a></td>
-                    <td class="info_separator"></td>
-                    <td>Linkedin:</td>
-                    <td><a href="{data_linkedin}">{data_linkedin}</a></td>
-                </tr>
-                    <td>Phone:</td>
-                    <td>{data_phone}</td>
-                    <td class="info_separator"></td>
-                    <td>{["", "GitHub:"][len(data_github)!=0]}</td>
-                    <td><a href="{data_github}">{data_github}</a></td>
-                <tr>
-                </tr>
-            </table>
+def html_contactinfo(data_email, data_phone, data_linkedin="", data_github=""):
+    if any([data_email, data_phone, data_linkedin, data_github]):
+        data_contactinfo_html = f'''
+                <table>
+                    <tr>
+                        <td>Email:</td>
+                        <td><a href="{data_email}">{data_email}</a></td>
+                        <td class="info_separator"></td>
+                        <td>Linkedin:</td>
+                        <!-- <td>{["", "Linkedin:"][len(data_linkedin)!=0]}</td> -->
+                        <td><a href="{data_linkedin}">{data_linkedin}</a></td>
+                    </tr>
+                        <td>Phone:</td>
+                        <td>{data_phone}</td>
+                        <td class="info_separator"></td>
+                        <td>{["", "GitHub:"][len(data_github)!=0]}</td>
+                        <td><a href="{data_github}">{data_github}</a></td>
+                    <tr>
+                    </tr>
+                </table>
 
+            '''
+        return f'''
+            <div class="contactinfo_container">
+                {data_contactinfo_html}
+            </div>
         '''
-    return f'''
-        <div class="contactinfo_container">
-            {data_contactinfo_html}
-        </div>
-    '''
 
 
 # OBJECTIVES
 # data_objectives = '''I am interested in the intersection between biology, cognition, and machine intelligence. 
 # My research experiences range from computational approaches such as machine perception and statistical modeling, to behavioral approaches such as field ethology with chimpanzees and operant conditioning with songbirds, to physiological approaches such as freely behaving ephys with songbirds and rodents.'''
 def html_objectives(data_objectives):
-    data_objectives_html = f'''
-            <div class="block">
-                {[data_objectives, data_objectives.replace("\n", "<br />")][data_objectives.find("\n") == -1]}
+    if data_objectives:
+        data_objectives_html = f'''
+                <div class="block">
+                    {[data_objectives, data_objectives.replace("\n", "<br />")][data_objectives.find("\n") == -1]}
+                </div>
+        '''
+        return f'''
+            <div class="bio">
+                {data_objectives_html}
             </div>
-    '''
-    return f'''
-        <div class="bio">
-            {data_objectives_html}
-        </div>
-    '''
+        '''
+    else:
+        return ""
 
 
 # RESEARCH INTERESTS
 # probably it's good idea to convert it to list of skills...
 def html_research(research):
-    data_research_html = f'''
-        <div class="block">
-            <p>
-                neuroethology,
-                neurophysiology/neural data analysis,
-                sensory/motor neuroscience,
-                language,
-                Animal cognition/communication,
-                sequence and generative models,
-                computational ethology,
-                machine perception & AI,
-                dimensionality reduction
-            </p>
-        </div>
-    '''
-    # with Research Interests part
-    # html_research = f'''
-    #     <h2>Research Interests</h2>
-    #     <div class="block">
-    #         {data_research_html}
-    #     </div>
-    # '''
-    # no research interest part
-    return ""
+    if research:
+        data_research_html = f'''
+            <div class="block">
+                <p>
+                    neuroethology,
+                    neurophysiology/neural data analysis,
+                    sensory/motor neuroscience,
+                    language,
+                    Animal cognition/communication,
+                    sequence and generative models,
+                    computational ethology,
+                    machine perception & AI,
+                    dimensionality reduction
+                </p>
+            </div>
+        '''
+        # with Research Interests part
+        # html_research = f'''
+        #     <h2>Research Interests</h2>
+        #     <div class="block">
+        #         {data_research_html}
+        #     </div>
+        # '''
+        # no research interest part
+    else:
+        return ""
 
 
 # EDUCATION
 def html_education(data_education):
-    data_education_html = []
-    for item in data_education:
-        item_html = f'''
-            <div class="item">
-                <div class="yearrange">
-                    {item["start"]}&#8211;{item["end"]}
+    if data_education:
+        data_education_html = []
+        for item in data_education:
+            item_html = f'''
+                <div class="item">
+                    <div class="yearrange">
+                        {item["start"]}&#8211;{item["end"]}
+                    </div>
+                    <div class="yearright">
+                        <strong>{item["name"]}</strong><br />
+                        {item["description"]}<br />
+                        {['', f'{item.get("thesis")}<br />'][item.get("thesis", 0)!=0]}
+                    </div>
                 </div>
-                <div class="yearright">
-                    <strong>{item["name"]}</strong><br />
-                    {item["description"]}<br />
-                    {['', f'{item.get("thesis")}<br />'][item.get("thesis", 0)!=0]}
-                </div>
+            '''
+            data_education_html.append(item_html)
+        data_education_html = "\n".join(data_education_html)
+        return f'''
+            <h2>Education</h2>
+            <div class="block">
+                {data_education_html}
             </div>
         '''
-        data_education_html.append(item_html)
-    data_education_html = "\n".join(data_education_html)
-    return f'''
-        <h2>Education</h2>
-        <div class="block">
-            {data_education_html}
-        </div>
-    '''
+    else:
+        return ""
 
 
 # ADDITIONAL COURSEWORK
 def html_coursework(data_coursework):
-    data_coursework_html = []
-    for item in data_coursework:
-        item_html = f'''
-            <div class="onelineitem">
-                <div class="coursework-year">
-                    {item["end"]}
+    if data_coursework:
+        data_coursework_html = []
+        for item in data_coursework:
+            item_html = f'''
+                <div class="onelineitem">
+                    <div class="coursework-year">
+                        {item["end"]}
+                    </div>
+                    <div class="coursework-item">
+                        {item["description"]}
+                    </div>
                 </div>
-                <div class="coursework-item">
-                    {item["description"]}
-                </div>
+            '''
+            data_coursework_html.append(item_html)
+        data_coursework_html = "\n".join(data_coursework_html)
+        return f'''
+            <h2>Additional Coursework</h2>
+            <div class="block">
+                {data_coursework_html}
             </div>
         '''
-        data_coursework_html.append(item_html)
-    data_coursework_html = "\n".join(data_coursework_html)
-    return f'''
-        <h2>Additional Coursework</h2>
-        <div class="block">
-            {data_coursework_html}
-        </div>
-    '''
+    else:
+        return ""
 
 
 # WORK EXPERIENCE
 def html_work(data_work):
-    # use data from data_work.py
-    # version for work description as a text ''' '''
-    # data_work_html = []
-    # for item in data_work:
-    #     item_html = f'''
-    #         <div class="onelineitem">
-    #             <strong>{item["start"]}&#8211;{item["end"]}&emsp;{item["job_title"]}&emsp;{item["name"]}</strong><br />
-    #             {item["description"].replace("\n", "<br />")}
-    #         </div>
-    #         '''
-    #     data_work_html.append(item_html)
-    # data_work_html = "\n".join(data_work_html)
+    if data_work:
+        # use data from data_work.py
+        # version for work description as a text ''' '''
+        # data_work_html = []
+        # for item in data_work:
+        #     item_html = f'''
+        #         <div class="onelineitem">
+        #             <strong>{item["start"]}&#8211;{item["end"]}&emsp;{item["job_title"]}&emsp;{item["name"]}</strong><br />
+        #             {item["description"].replace("\n", "<br />")}
+        #         </div>
+        #         '''
+        #     data_work_html.append(item_html)
+        # data_work_html = "\n".join(data_work_html)
 
-    # version for work description as a list with bullets
-    data_work_html = []
-    for item in data_work:
-        # convert description to list html code
-        list_html = []
-        for description_item in item["description"].split('\n'):
-            list_html.append(f"<li>{description_item}</li>")
-        list_html = "\n\t\t\t\t\t".join(list_html)
-        # html code for work
-        item_html = f'''
-            <div class="onelineitem">
-                <strong>{item["start"]}&#8211;{item["end"]}&emsp;{item["job_title"]}&emsp;{item["name"]}</strong><br />
-                <ul>
-                    {list_html}
-                </ul>
+        # version for work description as a list with bullets
+        data_work_html = []
+        for item in data_work:
+            # convert description to list html code
+            list_html = []
+            for description_item in item["description"].split('\n'):
+                list_html.append(f"<li>{description_item}</li>")
+            list_html = "\n\t\t\t\t\t".join(list_html)
+            # html code for work
+            item_html = f'''
+                <div class="onelineitem">
+                    <strong>{item["start"]}&#8211;{item["end"]}&emsp;{item["job_title"]}&emsp;{item["name"]}</strong><br />
+                    <ul>
+                        {list_html}
+                    </ul>
+                </div>
+                '''
+            data_work_html.append(item_html)
+
+        data_work_html = "\n".join(data_work_html)
+        return f'''
+            <h2>Experience</h2>
+            <div class="block">
+                {data_work_html}
             </div>
-            '''
-        data_work_html.append(item_html)
-
-    data_work_html = "\n".join(data_work_html)
-    return f'''
-        <h2>Experience</h2>
-        <div class="block">
-            {data_work_html}
-        </div>
-    '''
-    # print(data_work_html)
+        '''
+    else:
+        return ""
 
 
 # CONTRIBUTION
 def html_contributions(data_contributions):
-    data_contributions_html = []
-    for item in data_contributions:
-        item_html = f'''
-            <div class="contributions">
-                <span class="tt"><a href="{item["link"]}">{item["name"]}</a></span>,  {item["type"]}<br />
-                {item["description"]}
+    if data_contributions:
+        data_contributions_html = []
+        for item in data_contributions:
+            item_html = f'''
+                <div class="contributions">
+                    <span class="tt"><a href="{item["link"]}">{item["name"]}</a></span>,  {item["type"]}<br />
+                    {item["description"]}
+                </div>
+            '''
+            data_contributions_html.append(item_html)
+        data_contributions_html = "\n".join(data_contributions_html)
+        return f'''
+            <h2>Software Contributions</h2>
+            <div class="block">
+                {data_contributions_html}
             </div>
         '''
-        data_contributions_html.append(item_html)
-    data_contributions_html = "\n".join(data_contributions_html)
-    return f'''
-        <h2>Software Contributions</h2>
-        <div class="block">
-            {data_contributions_html}
-        </div>
-    '''
+    else:
+        return ""
 
 
 def html_ventures(data_ventures):
-        
-    '''
+    if data_ventures:
+        '''
         <h2>Employment and Business Ventures</h2>
 
         <div class="block">
@@ -244,7 +265,10 @@ def html_ventures(data_ventures):
             
         </div>
         '''
-    return f""
+        return f""
+    else:
+        return ""
+    
 
 
 # ANTI-BOT
@@ -275,7 +299,7 @@ def html_main(file_css, name, job_title, email, phone, github, linkedin, objecti
         
     <div id="cv_div">
         {html_header(name, job_title)}
-        {html_contactinfo(email, phone, github, linkedin)}
+        {html_contactinfo(email, phone, linkedin, github)}
         {html_objectives(objectives)}
         {html_research(researches)}
         {html_education(educations)}
