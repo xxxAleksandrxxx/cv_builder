372c8fd4ccbe4c2e7233f1e5d93215126f9c68f1
Aleksandr D <none>
2024-03-30 17:07:31 +0700

    update code, move executable part from cv_html_generator to main

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from research.cv_html_generator import build
+import os          # to to get current file path
+import webbrowser  # to open browser after cv is created
+
+from cv_html_generator import build
+from database.db_test import data as db
+
+
+file_css = "../css/cv_helion.css"
+file_cv = "output/cv.html"
+
 
 if __name__ == '__main__':
     try:
-        build()
-        print("Done")
+        build(
+            file_css,
+            file_cv,
+            db,
+            "Experiment & Test Engineer, Plasmas",
+            "th"  #th, uz_beeline, uz_mobile
+        )
+        webbrowser.open_new_tab("".join(["file:///", os.getcwd(), "/", file_cv]))
     except:
         print("Something happened and build() didn't run")
+        print(Exception)
