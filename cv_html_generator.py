527979e5d93953c617b4d180fce6067ed0059032
Aleksandr D <none>
2024-05-26 14:49:52 +0700

    rewrite string concatenation using list generator and "".json(); adopt skills part to list of strings.

--- cv_html_generator.py
+++ cv_html_generator.py
@@ -1,30 +1,3 @@
-# import os          # to to get current file path
-# import webbrowser  # to open browser after cv is created
-
-# import database.data_work as data_work
-# import database.data_coursework as data_coursework
-# import database.data_education as data_education
-# import database.data_contact_info as data_contact_info
-# import database.data_objectives as data_objectives
-# import database.data_contributions as data_contributions
-
-# from database.db_test import data as db
-
-
-# # CSS template to use in generated html
-# file_css = "../css/cv_helion.css"
-
-# # Folder to store generated html
-# file_cv = "output/cv.html"
-
-# data_name = data_contact_info.data["name"]
-# data_job_title = data_contact_info.data["job_title"]
-# data_email = data_contact_info.data["email"]
-# data_phone = data_contact_info.data["phone"]
-# data_github = data_contact_info.data["github"]
-# data_linkedin = data_contact_info.data["linkedin"]
-
-
 # NAME AND JOB TITLE
 def html_header(data_name="", data_job_title=""):
     if any([data_name, data_job_title]):
@@ -53,13 +26,15 @@ def html_contactinfo(data_email="", data_phone="", data_linkedin="", data_github
                         <td><a href="{data_email}">{data_email}</a></td>
                         <td class="info_separator"></td>
                         <!--  <td>Linkedin:</td>  -->
-                        <td>{["", "Linkedin:"][len(data_linkedin)!=0]}</td>
+                        <!-- <td>{["", "Linkedin:"][len(data_linkedin)!=0]}</td> -->
+                        <td>{"Linkedin:" if len(data_linkedin) != 0 else ""}</td>
                         <td><a href="{data_linkedin}">{data_linkedin}</a></td>
                     </tr>
                         <td>Phone:</td>
                         <td>{data_phone}</td>
                         <td class="info_separator"></td>
-                        <td>{["", "GitHub:"][len(data_github)!=0]}</td>
+                        <!-- <td>{["", "GitHub:"][len(data_github)!=0]}</td> -->
+                        <td>{"GitHub:" if len(data_github) != 0 else ""}</td>
                         <td><a href="{data_github}">{data_github}</a></td>
                     <tr>
                     </tr>
@@ -80,7 +55,8 @@ def html_objectives(data_objectives=""):
     if data_objectives:
         data_objectives_html = f'''
                 <div class="block">
-                    {[data_objectives, data_objectives.replace("\n", "<br />")][data_objectives.find("\n") == -1]}
+                    <!-- {[data_objectives, data_objectives.replace("\n", "<br />")][data_objectives.find("\n") == -1]} -->
+                    {data_objectives.replace("\n", "<br />") if data_objectives.find("\n") == -1 else data_objectives}
                 </div>
         '''
         return f'''
@@ -92,27 +68,101 @@ def html_objectives(data_objectives=""):
         return ""
 
 
+# # 1st iteration
+# # SKILLS
+# def html_skills(data_skills=[]):
+#     '''
+#     assume that data_skills is a list of strings
+#     '''
+#     if data_skills:
+    
+#         # version for work description as a list with bullets
+#         data_skills_html = []
+#         list_html = []
+#         for item in data_skills:
+#             list_html.append(f"<li>{item}</li>")
+#         list_html = "\n\t\t\t\t\t".join(list_html)
+#         data_skills_html = f'''
+#             <div class="onelineitem">
+                
+#                 <ul>
+#                     {list_html}
+#                 </ul>
+#             </div>
+#             '''
+#         return f'''
+#             <h2>Skills</h2>
+#             <div class="block">
+#                 {data_skills_html}
+#             </div>
+#         '''
+#     else:
+#         return ""
+
+
+# # version for skills being in one line string separated with ", "
+# # SKILLS
+# def html_skills(data_skills=[]):
+#     '''
+#     assume that data_skills is a list of strings
+#     '''
+#     if data_skills:
+#         data_skills_html = []
+#         list_html = []
+#         for item in data_skills:
+#             list_html.append(f"<p>{item}</p>")
+#         list_html = "\n\t\t\t\t\t".join(list_html)
+#         data_skills_html = f'''
+#             <div class="onelineitem">
+                
+#                 <!-- <p> -->
+#                     {list_html}
+#                 <!-- </p> -->
+#             </div>
+#             '''
+#         return f'''
+#             <h2>Skills</h2>
+#             <div class="block">
+#                 {data_skills_html}
+#             </div>
+#         '''
+#     else:
+#         return ""
+    
+
+# # with list generator and "".join()
+# # version for skills being in one line string separated with ", "
+# # SKILLS
+# def html_skills(data_skills=""):
+#     '''
+#     assume that data_skills is a string
+#     '''
+#     if data_skills:
+#         data_skills_html = f'''
+#                 {data_skills}
+#             '''
+#         return f'''
+#             <h2>Skills</h2>
+#             <div class="block">
+#                 {data_skills_html}
+#             </div>
+#         '''
+#     else:
+#         return ""
+
+
+# with list generator and "".join()
+# version for skills being in a list
 # SKILLS
 def html_skills(data_skills=[]):
     '''
-    assume that data_skills is a list of strings
+    assume that data_skills is a string
     '''
     if data_skills:
-    
-        # version for work description as a list with bullets
-        data_skills_html = []
-        list_html = []
-        for item in data_skills:
-            list_html.append(f"<li>{item}</li>")
-        list_html = "\n\t\t\t\t\t".join(list_html)
-        data_skills_html = f'''
-            <div class="onelineitem">
-                
-                <ul>
-                    {list_html}
-                </ul>
-            </div>
-            '''
+        data_skills_html = ", ".join(
+            f'{item}'
+            for item in data_skills
+        )
         return f'''
             <h2>Skills</h2>
             <div class="block">
@@ -127,53 +177,76 @@ def html_skills(data_skills=[]):
 # probably it's good idea to convert it to list of skills...
 def html_research(research=""):
     if research:
-        data_research_html = f'''
-            <div class="block">
-                <p>
-                    neuroethology,
-                    neurophysiology/neural data analysis,
-                    sensory/motor neuroscience,
-                    language,
-                    Animal cognition/communication,
-                    sequence and generative models,
-                    computational ethology,
-                    machine perception & AI,
-                    dimensionality reduction
-                </p>
-            </div>
-        '''
-        # with Research Interests part
-        # html_research = f'''
-        #     <h2>Research Interests</h2>
+        # data_research_html = f'''
         #     <div class="block">
-        #         {data_research_html}
+        #         <p>
+        #             neuroethology,
+        #             neurophysiology/neural data analysis,
+        #             sensory/motor neuroscience,
+        #             language,
+        #             Animal cognition/communication,
+        #             sequence and generative models,
+        #             computational ethology,
+        #             machine perception & AI,
+        #             dimensionality reduction
+        #         </p>
         #     </div>
         # '''
-        # no research interest part
         return ""
     else:
         return ""
 
 
+# # EDUCATION
+# def html_education(data_education=""):
+#     if data_education:
+#         data_education_html = []
+#         for item in data_education:
+#             item_html = f'''
+#                 <div class="item">
+#                     <div class="yearrange">
+#                         {item["start"]}&#8211;{item["end"]}
+#                     </div>
+#                     <div class="yearright">
+#                         <strong>{item["name"]}</strong><br />
+#                         {item["description"]}<br />
+#                         {['', f'{item.get("thesis")}<br />'][item.get("thesis", 0)!=0]}
+#                     </div>
+#                 </div>
+#             '''
+#             data_education_html.append(item_html)
+#         data_education_html = "\n".join(data_education_html)
+#         return f'''
+#             <h2>Education</h2>
+#             <div class="block">
+#                 {data_education_html}
+#             </div>
+#         '''
+#     else:
+#         return ""
+
+
+# with list generator and "".join()
 # EDUCATION
 def html_education(data_education=""):
     if data_education:
-        data_education_html = []
-        for item in data_education:
-            item_html = f'''
-                <div class="item">
-                    <div class="yearrange">
-                        {item["start"]}&#8211;{item["end"]}
-                    </div>
-                    <div class="yearright">
-                        <strong>{item["name"]}</strong><br />
-                        {item["description"]}<br />
-                        {['', f'{item.get("thesis")}<br />'][item.get("thesis", 0)!=0]}
-                    </div>
+        # data_education_html = []
+        # for item in data_education:
+        data_education_html = "\n".join(
+            f'''
+            <div class="item">
+                <div class="yearrange">
+                    {item["start"]}&#8211;{item["end"]}
                 </div>
+                <div class="yearright">
+                    <strong>{item["name"]}</strong><br />
+                    {item["description"]}<br />
+                    {f'{item.get("thesis")}<br />' if item.get("thesis", 0) != 0 else ""}
+                </div>
+            </div>
             '''
-            data_education_html.append(item_html)
-        data_education_html = "\n".join(data_education_html)
+            for item in data_education
+        )
         return f'''
             <h2>Education</h2>
             <div class="block">
@@ -184,23 +257,50 @@ def html_education(data_education=""):
         return ""
 
 
+# # ADDITIONAL COURSEWORK
+# def html_coursework(data_coursework=""):
+#     if data_coursework:
+#         data_coursework_html = []
+#         for item in data_coursework:
+#             item_html = f'''
+#                 <div class="onelineitem">
+#                     <div class="coursework-year">
+#                         {item["end"]}
+#                     </div>
+#                     <div class="coursework-item">
+#                         {item["description"]}
+#                     </div>
+#                 </div>
+#             '''
+#             data_coursework_html.append(item_html)
+#         data_coursework_html = "\n".join(data_coursework_html)
+#         return f'''
+#             <h2>Additional Coursework</h2>
+#             <div class="block">
+#                 {data_coursework_html}
+#             </div>
+#         '''
+#     else:
+#         return ""
+
+
+# with list generator and "".join()
 # ADDITIONAL COURSEWORK
 def html_coursework(data_coursework=""):
     if data_coursework:
-        data_coursework_html = []
-        for item in data_coursework:
-            item_html = f'''
-                <div class="onelineitem">
-                    <div class="coursework-year">
-                        {item["end"]}
-                    </div>
-                    <div class="coursework-item">
-                        {item["description"]}
-                    </div>
+        data_coursework_html = "\n".join(
+            f'''
+            <div class="onelineitem">
+                <div class="coursework-year">
+                    {item["end"]}
                 </div>
+                <div class="coursework-item">
+                    {item["description"]}
+                </div>
+            </div>
             '''
-            data_coursework_html.append(item_html)
-        data_coursework_html = "\n".join(data_coursework_html)
+            for item in data_coursework
+        )
         return f'''
             <h2>Additional Coursework</h2>
             <div class="block">
